9f83954be6caed9c00beb076383a76931f79fe69
ramostear <ramostear@163.com>
2020-04-13 23:46:21 +0800

    10 minutes write a beautiful responsive web login page

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunction(){
+    let parentNode = this.parentNode.parentNode;
+    parentNode.classList.add('focus');
+}
+function blurFunction(){
+    let parentNode = this.parentNode.parentNode;
+    if(this.value == ''){
+        parentNode.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input=>{
+    input.addEventListener('focus',focusFunction);
+    input.addEventListener('blur',blurFunction);
+});
+
